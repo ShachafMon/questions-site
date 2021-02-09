467c239a8b509e3f6e132d83e92820f1874f17c7
Shachaf <Shahaf.mon@gmail.com>
2021-02-09 14:51:18 +0200

    added wrong url redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './questions-site/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path:'login', component: LoginComponent}
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/questions-site/site-routing.module.ts
+++ src/app/questions-site/site-routing.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginGuardGuard } from '../shared/guards/login-guard.guard';
 import { ChartsComponent } from './charts/charts.component';
+import { LoginComponent } from './login/login.component';
 import { ManagerComponent } from './manager/manager.component';
 import { QuestionListComponent } from './manager/question-list/question-list.component';
 
 
 const routes: Routes = [
+
+  { path: 'login', component: LoginComponent }
+  ,
   {
     path: 'manager', component: ManagerComponent, children:
       [
         { path: '', redirectTo: 'questions', pathMatch: 'full' },
         { path: 'questions', component: QuestionListComponent },
-        { path: 'charts', component: ChartsComponent }
-      ],canActivate:[LoginGuardGuard]
-  }
+        { path: 'charts', component: ChartsComponent },
+        { path: '**', redirectTo: 'questions', pathMatch: 'full' }
+      ], canActivate: [LoginGuardGuard]
+  },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
